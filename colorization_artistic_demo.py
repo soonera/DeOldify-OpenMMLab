90f96c10e7ae3d5aa6263994a679429dd3892286
SENSETIME\renqin <1072144920@qq.com>
2021-10-26 13:22:23 +0800

    add artistic model

---
+++ colorization_artistic_demo.py
@@ -0,0 +1,41 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import argparse
+
+import torch
+
+from apis.colorization_inference import colorization_inference, init_colorization_model
+from glob import glob
+from os.path import join
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='colorization demo')
+    parser.add_argument('config', help='test config file path')
+    parser.add_argument('checkpoint', help='checkpoint file')
+    parser.add_argument('work_dir', help='path to input image file')
+    # parser.add_argument('save_path', help='path to save colorization result')
+    # parser.add_argument(
+    #     '--imshow', action='store_true', help='whether show image with opencv')
+    parser.add_argument('--device', type=int, default=0, help='CUDA device id')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    model = init_colorization_model(
+        args.config, args.checkpoint, device=torch.device('cuda', args.device))
+
+    img_paths = sorted(glob(join(args.work_dir, "sources/*.*")))
+    for img_path in img_paths:
+
+        output = colorization_inference(model, img_path)
+
+        file_name = img_path.split('/')[-1].split('.')[0]
+        output.save(join(args.work_dir, "results", file_name + '.png'))
+        print(file_name)
+
+
+if __name__ == '__main__':
+    main()
---
+++ configs/deoldify_artistic_configs.py
@@ -0,0 +1,64 @@
+# custom_imports = dict(imports=['my_pipelines'], allow_failed_imports=False)
+import sys
+# sys.path.append('/home/SENSETIME/renqin/PycharmProjects/DeOldify-demo/configs')
+sys.path.append('/home/SENSETIME/renqin/PycharmProjects/DeOldify-demo/models')
+sys.path.append('/home/SENSETIME/renqin/PycharmProjects/DeOldify-demo/apis')
+sys.path.append('/home/SENSETIME/renqin/PycharmProjects/DeOldify-demo/datasets')
+
+
+custom_imports = dict(
+    imports=['deoldify', 'resnet_backbone', 'channels_from_one_to_three'],
+    allow_failed_imports=False)
+
+model = dict(
+    type='DynamicUnetDeep',
+    encoder=dict(
+        type='ColorizationResNet',
+        num_layers=34,
+        pretrained=None),
+    nf_factor=1.5
+    )
+
+
+train_cfg = dict()
+test_cfg = dict()
+
+
+dataset_type = ''
+
+
+train_pipeline = []
+test_pipeline = [
+    dict(
+        type='LoadImageFromFile',
+        key='gt_img',
+        flag='grayscale',
+        backend='pillow'
+    ),
+    dict(
+        type='Resize',
+        keys=['gt_img'],
+        scale=(35 * 16, 35 * 16),
+        keep_ratio=False,
+        backend='pillow'
+    ),
+    dict(type='RescaleToZeroOne', keys=['gt_img']),
+    dict(
+        type='ChannelsFromOneToThree',
+        keys=['gt_img'],
+    ),
+    dict(
+        type='Normalize',
+        keys=['gt_img'],
+        mean=[0.4850, 0.4560, 0.4060],
+        std=[0.2290, 0.2240, 0.2250],
+        to_rgb=False),
+    dict(
+        type='Collect',
+        keys=['gt_img'],
+        meta_keys=['gt_img_path']),
+    dict(type='ImageToTensor',
+         keys=['gt_img']),
+]
+
+
